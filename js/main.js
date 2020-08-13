1b64492bb756912bd0d4dff5833610b7e277171e
fafatim <farhana.fatima.lima@gmail.com>
2020-08-12 23:21:39 -0400

    home page done for mobile version

--- js/main.js
+++ js/main.js
@@ -5,3 +5,29 @@ var typed2 = new Typed('#typed', {
   fadeOut: true,
   loop: true
 });
+//navbar action
+// var menuToggle = document.getElementById("menuToggle");
+// var crossButton = document.getElementById("cross-button");
+// var navBar = document.getElementById("navBarMenu");
+// function changeNav(){
+//   if (navBar.style.display === "block"){
+//     navBar.style.display = "none";
+//   }
+//   else {
+//     navBar.style.display = "block";
+//   }
+// }
+// menuToggle.addEventListener("click",changeNav);
+// crossButton.addEventListener("click",changeNav);
+
+var cssProperties = { width: '100%' };
+var cssProperties1 = { width: '0%' };
+
+$( "#menuToggle" ).click(function() {
+  $(".navbar").show().animate(cssProperties);
+});
+$( "#cross-button" ).click(function() {
+  $(".navbar").animate(cssProperties1,function(){
+    $(this).hide();
+  });
+});
