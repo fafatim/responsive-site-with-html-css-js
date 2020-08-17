01ecbd57b0987eb7892ca959d91667188910f37c
fafatim <farhana.fatima.lima@gmail.com>
2020-08-17 13:08:03 -0400

    personal info and my services done for about page

--- js/script.js
+++ js/script.js
@@ -44,6 +44,26 @@ $( ".cross-button" ).click(function() {
 
   });
 });
+var sections = $('section')
+  , nav = $('#mySidenav')
+  , nav_height = nav.outerHeight();
+
+$(window).on('scroll', function () {
+  var cur_pos = $(this).scrollTop();
+
+  sections.each(function() {
+    var top = $(this).offset().top - nav_height -300,
+      bottom = top + $(this).outerHeight();
+
+    if (cur_pos >= top && cur_pos <= bottom) {
+      nav.find('a').removeClass('active');
+      sections.removeClass('active');
+
+      $(this).addClass('active');
+      nav.find('a[href="#'+$(this).attr('id')+'"]').addClass('active');
+    }
+  });
+});
 // var cssProperties2 = { width: '25%' };
 // var cssProperties3 = { width: '0%' };
 //
